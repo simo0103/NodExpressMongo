5ac00893958399f7308bf04f139b8e5c9feb6f76
Simona Trotti <simona.trotti.external@ynap.com>
2019-06-14 17:20:42 +0200

    printing datas

--- models/db.js
+++ models/db.js
@@ -2,6 +2,11 @@ const MONGOOSE = require ('mongoose');
 
 MONGOOSE.connect('mongodb://localhost:27017/MenuDB', {useNewUrlParser: true}, (err, db) => {
     !err ? console.log('MONGODB CONNECTED!!!') : console.log('ERROR!DB NOT CONNECTED! ' + err)
-    db.close()
+    var cursor = db.collection('menu').find();
+
+    cursor.each(function(err, doc) {
+
+        console.log(doc);
+    });
 });
 
