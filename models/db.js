b4a990661d208f8270e6a5f7696d265facf35ab3
Simona Trotti <simona.trotti.external@ynap.com>
2019-06-14 17:02:55 +0200

    how to modified

--- models/db.js
+++ models/db.js
@@ -1,6 +1,7 @@
 const MONGOOSE = require ('mongoose');
 
-MONGOOSE.connect('mongodb://localhost:27017/MenuDB', {useNewUrlParser: true}, (err) => {
+MONGOOSE.connect('mongodb://localhost:27017/MenuDB', {useNewUrlParser: true}, (err, db) => {
     !err ? console.log('MONGODB CONNECTED!!!') : console.log('ERROR!DB NOT CONNECTED! ' + err)
+    db.close()
 });
 
